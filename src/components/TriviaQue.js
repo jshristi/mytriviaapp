291474b683b96fc8c98a3fd16e321873d7ccec41
Shristi Jha <jshristi17@gmail.com>
2023-07-06 23:26:40 +0530

    UI changes

--- src/components/TriviaQue.js
+++ src/components/TriviaQue.js
@@ -1,21 +1,18 @@
 import classes from "../UI/TriviaQue.module.css";
 
-function TriviaQue({que}) {
- 
-    return (
-        <div className={classes.container}>                 
-            <span className={classes.card}>                   
-                <div className={classes.category}>
-                    {que.category}
-                </div>
-                <div className={classes.level}>
-                    Level: {que.difficulty}  
-                </div>
-            </span> 
-            <h4>{que.question.replaceAll('&quot;', '"').replaceAll('&#039;', "'")}</h4>
-            <h5>Answer: {que.correct_answer}</h5>                 
-        </div>
-    )
-};
+function TriviaQue({ que }) {
+  return (
+    <div className={classes.container}>
+      <span className={classes.card}>
+        <div className={classes.category}>{que.category}</div>
+        <div className={classes.level}>Level: {que.difficulty} </div>
+      </span>
+      <h4>
+        {que.question.replaceAll("&quot;", '"').replaceAll("&#039;", "'")}
+      </h4>
+      <h5>Answer: {que.correct_answer}</h5>
+    </div>
+  );
+}
 
 export default TriviaQue;
