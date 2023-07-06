4d4f3d894816cebc5c3a0c6623d9e9631a2f961b
Shristi Jha <jshristi17@gmail.com>
2023-07-06 23:18:30 +0530

    1. UI changes 2. Formats question text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import TriviaQue from './components/TriviaQue';
 import {useState} from "react";
 import Axios from "axios";
 import classes from './UI/App.module.css';
-import useAlan from '../../my-trivia/src/components/useAlan';
+import useAlan from './components/useAlan';
+
 
 function App() {
   const [que, setQue] = useState('');        
@@ -10,27 +11,25 @@ function App() {
   const getQue = num  => {
     let q ='';
     Axios.get('https://opentdb.com/api.php?amount=1&category='+ num +'&type=multiple').then((response) => {
-         console.log("response:",response.data.results[0].question);
           q = response.data.results[0].question;
          setQue(response.data.results[0]);    
-         console.log("ingetque:"+q)
     })
        
   }
   
   useAlan({getQue});
   return (
-    <div className={classes.card}>
-      <center>
-        <h2>Welcome To Trivia Quiz </h2>
-        <h3>Select any category!</h3>
-      </center>
-      <div className={classes.catBtn}>
-          <button onClick={() => getQue('10')}>Book</button>
-          <button onClick={() => getQue('12')}>Music</button>
-          <button onClick={() => getQue('11')}>Film</ button>                
+    <div className={classes.container}>
+      <div className={classes.card}>
+          <h1>Welcome To Trivia Quiz </h1>
+          <h4>Select any category!</h4>
+        <div className={classes.catBtn}>
+            <button onClick={() => getQue('10')}>Book</button>
+            <button onClick={() => getQue('12')}>Music</button>
+            <button onClick={() => getQue('11')}>Film</ button>                
+        </div>
+        {que && <TriviaQue que={que} /> }
       </div>
-      {que && <TriviaQue que={que} /> }
     </div>
   );
 }
--- src/components/TriviaQue.js
+++ src/components/TriviaQue.js
@@ -1,20 +1,19 @@
 import classes from "../UI/TriviaQue.module.css";
 
 function TriviaQue({que}) {
+ 
     return (
-        <div className={classes.card}>                 
-            <div className={classes.newsMaterial}>    
-                <div className={classes.container}>                   
-                    <div className={classes.category}>
-                        {que.category}
-                    </div>
-                    <div className={classes.level}>
-                        Level: {que.difficulty}  
-                    </div>
-                </div> 
-                <h4>{que.question}</h4>
-                <h5>Answer: {que.correct_answer}</h5>                 
-            </div>
+        <div className={classes.container}>                 
+            <span className={classes.card}>                   
+                <div className={classes.category}>
+                    {que.category}
+                </div>
+                <div className={classes.level}>
+                    Level: {que.difficulty}  
+                </div>
+            </span> 
+            <h4>{que.question.replaceAll('&quot;', '"').replaceAll('&#039;', "'")}</h4>
+            <h5>Answer: {que.correct_answer}</h5>                 
         </div>
     )
 };
